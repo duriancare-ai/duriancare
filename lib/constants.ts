516aba350750da5d08fd5c2a8dd813d844e685a4
duriancare-ai <duriancare@gmail.com>
2026-06-11 21:01:19 +0800

    add 07

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // --- APP CONFIGURATION ---
-export const RESTRICTION_DATE = "2026-07-05T23:59:00";
+export const RESTRICTION_DATE = "2026-08-05T23:59:00";
 export const SUPPORT_CONTACT = "+63 909 180 4520";
 export const SYSTEM_ID = "DC-APP-0001";
 
