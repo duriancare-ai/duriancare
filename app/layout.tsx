540cad88c1148fd9a3f12f6f17e3e6e18697fc52
duriancare-ai <duriancare@gmail.com>
2026-06-16 18:34:01 +0800

    add space

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import BottomNav from "../components/BottomNav";
 import TopNav from "../components/TopNav";
 import SyncProvider from "../components/SyncProvider";
-import PaymentLock from "../components/PaymentLock";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -47,24 +46,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-100 h-full overflow-hidden`}>
-        <PaymentLock>
-          <SyncProvider>
-            <div className="relative h-full max-w-md mx-auto bg-white shadow-2xl flex flex-col">
-              {/* Top Navigation */}
-              <TopNav />
-              
-              {/* Scrollable Content Area */}
-              <main className="flex-1 overflow-y-auto overflow-x-hidden scroll-smooth">
-                {children}
-              </main>
-
-              {/* Bottom Navigation */}
-              <BottomNav />
-              
-              {/* Global Overlays */}
-            </div>
-          </SyncProvider>
-        </PaymentLock>
+       
       </body>
     </html>
   );
